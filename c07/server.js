1021cf0834c946a981a1ad06dd7d09679a99adb0
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-15 11:41:10 +0300

    terminat c07

---
+++ c07/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3030;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  switch (req.url) {
+    case '/':
+      res.end('Home Page');
+      break;
+    case '/about':
+      res.end('About Page');
+      break;
+    case '/contact':
+      res.end('Contact Page');
+      break;
+    default:
+      res.end('Not found');
+      break;
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
